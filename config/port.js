55f44b87ec9b9dc035431896c45a74512874a2bd
Dimitar Todorov <68326151+dimitartodorovv@users.noreply.github.com>
2022-10-27 15:22:04 +0300

    Update port.js

--- config/port.js
+++ config/port.js
@@ -4,7 +4,7 @@ const config = {
     },
     production: {
         port: 80,
-        mongooCN: `mongodb+srv://Dimitar:1q2w3e4r@cluster0.1atc3.mongodb.net/docCon?retryWrites=true&w=majority`
+        mongooCN: ``
     } 
 
 }
